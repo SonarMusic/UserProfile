659024b979d6355208e836ac9d19b5217673c5d8
Valery Shevchenko <ar4er.valera@gmail.com>
2022-05-21 18:48:15 +0300

    feat: not implemented user profile controller

---
+++ Sonar.UserProfile.Web/Controllers/UserProfile/Dto/UserProfileDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Sonar.UserProfile.Web.Controllers.UserProfile.Dto;
+
+public class UserProfileDto
+{
+    public Guid Id { get; set; }
+    public string Login { get; set; }
+}
---
+++ Sonar.UserProfile.Web/Controllers/UserProfile/UserProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sonar.UserProfile.Web.Controllers.UserProfile.Dto;
+
+namespace Sonar.UserProfile.Web.Controllers.UserProfile;
+
+[ApiController]
+[Route("[controller]")]
+public class UserProfileController : ControllerBase
+{
+    [HttpPatch("login/{id:guid}")]
+    public Task Login(Guid id, string password, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPatch("logout/{id:guid}")]
+    public Task Logout(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet("{id:guid}")]
+    public Task<UserProfileDto> Get(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
