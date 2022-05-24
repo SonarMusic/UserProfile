4c7b0e9bf6c2de9348b3b63e277a74656265554b
Aedilay <Edgar.saratovcev@gmail.com>
2022-05-24 16:05:42 +0300

    feat: add BCryptEncoder

---
+++ Sonar.UserProfile.Core/Domain/Users/Encoders/IPasswordEncoder.cs
@@ -0,0 +1,8 @@
+﻿namespace Sonar.UserProfile.Core.Domain.Users.Encoders;
+
+public interface IPasswordEncoder
+{
+    public string GetRandomSalt();
+    public string Encode(string rawPassword);
+    public bool Matches(string rawPassword, string hashedPassword);
+}
---
+++ Sonar.UserProfile.Data/Users/Encoders/BCryptPasswordEncoder.cs
@@ -0,0 +1,21 @@
+﻿using Sonar.UserProfile.Core.Domain.Users.Encoders;
+namespace Sonar.UserProfile.Data.Users.Encoders;
+
+public class BCryptPasswordEncoder : IPasswordEncoder
+{
+    public string GetRandomSalt()
+    {
+        return BCrypt.Net.BCrypt.GenerateSalt(12);
+    }
+
+    public string Encode(string rawPassword)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(rawPassword);
+
+    }
+
+    public bool Matches(string rawPassword, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(rawPassword, hashedPassword);
+    }
+}
