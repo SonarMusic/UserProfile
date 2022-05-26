86c20ad24d3787dbc3da3bdf07935257591d7d10
Aedilay <Edgar.saratovcev@gmail.com>
2022-05-26 19:22:46 +0300

    feat: add custom exceptions

---
+++ Sonar.UserProfile.Core/Domain/Exceptions/ExpiredTokenException.cs
@@ -0,0 +1,18 @@
+﻿namespace Sonar.UserProfile.Core.Domain.Exceptions;
+
+public class ExpiredTokenException : Exception
+{
+    public ExpiredTokenException()
+    {
+    }
+    
+    public ExpiredTokenException(string message) 
+        : base(message)
+    {
+    }
+
+    public ExpiredTokenException(string message, Exception e)
+        : base(message, e)
+    {
+    }
+}
---
+++ Sonar.UserProfile.Core/Domain/Exceptions/IAmTeapotException.cs
@@ -0,0 +1,18 @@
+﻿namespace Sonar.UserProfile.Core.Domain.Exceptions;
+
+public class IAmTeapotException : Exception
+{
+    public IAmTeapotException()
+    {
+    }
+    
+    public IAmTeapotException(string message) 
+        : base(message)
+    {
+    }
+
+    public IAmTeapotException(string message, Exception e)
+        : base(message, e)
+    {
+    }
+}
