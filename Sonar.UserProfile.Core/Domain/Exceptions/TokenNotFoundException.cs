69de678217a8ab5752eaedb49ca1b4b8c884f719
Aedilay <Edgar.saratovcev@gmail.com>
2022-05-26 19:38:50 +0300

    feat: add TokenNotFound exception

---
+++ Sonar.UserProfile.Core/Domain/Exceptions/TokenNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace Sonar.UserProfile.Core.Domain.Exceptions;
+
+public class TokenNotFoundException : Exception
+{
+    public TokenNotFoundException()
+    {
+    }
+    
+    public TokenNotFoundException(string message) 
+        : base(message)
+    {
+    }
+
+    public TokenNotFoundException(string message, Exception e)
+        : base(message, e)
+    {
+    }
+}
