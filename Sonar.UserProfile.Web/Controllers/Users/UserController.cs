986a96eb963c8e8176d1edded8f6b60acbca5510
Valery Shevchenko <ar4er.valera@gmail.com>
2022-05-30 20:01:19 +0300

    doc: response codes

--- Sonar.UserProfile.Web/Controllers/Users/UserController.cs
+++ Sonar.UserProfile.Web/Controllers/Users/UserController.cs
@@ -16,14 +16,18 @@ public class UserController : ControllerBase
     {
         _userService = userService;
     }
-    
+
     /// <summary>
     /// Create new user with new token which will expire in 7 days.
     /// </summary>
-    /// <param name="userRegisterDto">Contains parameters for new user: email, password.</param>
-    /// <param name="cancellationToken"></param>
-    /// <returns>Token (ID of generated token to be precise).</returns>
+    /// <param name="userRegisterDto">DTO which contains parameters for new user: email, password.</param>
+    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
+    /// <returns>New token (ID of generated token to be precise).</returns>
+    /// <response code="200">OK</response>
+    /// <response code="500">Internal server exception.</response>
     [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public Task<Guid> Register(UserRegisterDto userRegisterDto, CancellationToken cancellationToken = default)
     {
         var user = new User
@@ -38,12 +42,18 @@ public class UserController : ControllerBase
     /// <summary>
     /// Generate new token to user if password matched. Token will expire in 7 days.
     /// </summary>
-    /// <param name="userLoginDto">Contains parameters to identify user: email, password</param>
-    /// <param name="cancellationToken"></param>
-    /// <returns>Token (ID of generated token to be precise).</returns>
-    /// <exception cref="InvalidPasswordException">Throw if password didn't match.</exception>
-    /// <exception cref="UserNotFoundException">Throw if user with such ID doesn't exist.</exception>
+    /// <param name="userLoginDto">DTO which contains parameters to identify user: email, password</param>
+    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
+    /// <returns>New token (ID of generated token to be precise).</returns>
+    /// <response code="200">OK</response>
+    /// <response code="401">Password didn't match.</response>
+    /// <response code="404">User with such ID doesn't exist.</response>
+    /// <response code="500">Internal server exception.</response>
     [HttpPatch("login")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public Task<Guid> Login(UserLoginDto userLoginDto, CancellationToken cancellationToken = default)
     {
         var user = new User
@@ -56,16 +66,24 @@ public class UserController : ControllerBase
     }
 
     /// <summary>
-    /// Expire token (delete it).
+    /// Delete token.
     /// </summary>
-    /// <param name="tokenHeader">Contains token (ID of token to be precise).</param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="tokenHeader">DTO which contains token (ID of token to be precise).</param>
+    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
     /// <returns>Task</returns>
-    /// <exception cref="InvalidRequestException">Throw if header doesn't contain token.</exception>
-    /// <exception cref="ExpiredTokenException">Throw if token already has expired.</exception>
-    /// <exception cref="UserNotFoundException">Throw if user with such ID doesn't exist.</exception>
-    /// <exception cref="TokenNotFoundException">Throw if token with such ID doesn't exist.</exception>
+    /// <response code="200">OK</response>
+    /// <response code="400">Header doesn't contain token.</response>
+    /// <response code="401">Password didn't match.</response>
+    /// <response code="403">Token already has expired.</response>
+    /// <response code="404">Entity with such ID doesn't exist.</response>
+    /// <response code="500">Internal server exception.</response>
     [HttpPatch("logout")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public Task Logout([FromHeader(Name = "Token")] string tokenHeader, CancellationToken cancellationToken = default)
     {
         if (tokenHeader is null)
@@ -82,13 +100,19 @@ public class UserController : ControllerBase
     /// Return a user model if token hasn't expired yet.
     /// </summary>
     /// <param name="tokenHeader">Contains token (ID of token to be precise).</param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the task to complete.</param>
     /// <returns>User model which contains: ID, email.</returns>
-    /// <exception cref="InvalidRequestException">Throw if header doesn't contain token.</exception>
-    /// <exception cref="ExpiredTokenException">Throw if token already has expired.</exception>
-    /// <exception cref="UserNotFoundException">Throw if user with such ID doesn't exist.</exception>
-    /// <exception cref="TokenNotFoundException">Throw if token with such ID doesn't exist.</exception>
+    /// <response code="200">OK</response>
+    /// <response code="400">Header doesn't contain token.</response>
+    /// <response code="403">Token already has expired.</response>
+    /// <response code="404">Entity with such ID doesn't exist.</response>
+    /// <response code="500">Internal server exception.</response>
     [HttpGet("get")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<UserGetDto> Get([FromHeader(Name = "Token")] string tokenHeader, CancellationToken cancellationToken = default)
     {
         if (tokenHeader is null)
