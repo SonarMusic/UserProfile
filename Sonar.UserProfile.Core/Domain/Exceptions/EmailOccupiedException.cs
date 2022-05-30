2c65b17d35748750ff32297f7cff70df54a30cd5
Aedilay <Edgar.saratovcev@gmail.com>
2022-05-30 19:09:14 +0300

    feat: add EmailOccupiedException

---
+++ Sonar.UserProfile.Core/Domain/Exceptions/EmailOccupiedException.cs
@@ -0,0 +1,18 @@
+﻿namespace Sonar.UserProfile.Core.Domain.Exceptions;
+
+public class EmailOccupiedException : Exception
+{
+    public EmailOccupiedException()
+    {
+    }
+    
+    public EmailOccupiedException(string message) 
+        : base(message)
+    {
+    }
+
+    public EmailOccupiedException(string message, Exception e)
+        : base(message, e)
+    {
+    }
+}
