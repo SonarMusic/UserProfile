6eb8cdb2fbc9830c9992f65c52d5a5ac9369c317
Aedilay <Edgar.saratovcev@gmail.com>
2022-06-02 00:43:54 +0300

    feat: add custom error messages to validation

--- Sonar.UserProfile.Web/Controllers/Users/Dto/UserRegisterDto.cs
+++ Sonar.UserProfile.Web/Controllers/Users/Dto/UserRegisterDto.cs
@@ -4,11 +4,11 @@ namespace Sonar.UserProfile.Web.Controllers.Users.Dto;
 
 public class UserRegisterDto
 {
-    [Required]
-    [RegularExpression(".+@.+\\..+")]
+    [Required(ErrorMessage = "Email is required")]
+    [RegularExpression(".+@.+\\..+", ErrorMessage = "Email is incorrect")]
     public string Email { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Password is required")]
     // Считаю, что пороль меньше 6 символов не может считаться паролем априори
-    [StringLength(6)]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
     public string Password { get; set; }
 }
