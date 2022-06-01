5e51e56571f48d72c34e45b34b57cd0e135bfc04
Aedilay <Edgar.saratovcev@gmail.com>
2022-06-02 00:31:10 +0300

    feat: add validation to UserRegisterDto

--- Sonar.UserProfile.Web/Controllers/Users/Dto/UserRegisterDto.cs
+++ Sonar.UserProfile.Web/Controllers/Users/Dto/UserRegisterDto.cs
@@ -1,7 +1,14 @@
-﻿namespace Sonar.UserProfile.Web.Controllers.Users.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sonar.UserProfile.Web.Controllers.Users.Dto;
 
 public class UserRegisterDto
 {
+    [Required]
+    [RegularExpression(".+@.+\\..+")]
     public string Email { get; set; }
+    [Required]
+    // Считаю, что пороль меньше 6 символов не может считаться паролем априори
+    [StringLength(6)]
     public string Password { get; set; }
 }
