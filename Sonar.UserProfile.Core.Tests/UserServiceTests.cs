b0540be60fd6cd413d8174530a8c9134b93d32b3
nikolaykochubeev <nikolay.kochubeev@gmail.com>
2022-06-01 06:26:14 +0300

    chore: initial commit

---
+++ Sonar.UserProfile.Core.Tests/UserServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Moq;
+using Sonar.UserProfile.Core.Domain.Tokens.Repositories;
+using Sonar.UserProfile.Core.Domain.Users.Repositories;
+using Sonar.UserProfile.Core.Domain.Users.Services;
+using Xunit;
+
+namespace Sonar.UserProfile.Core.Tests;
+
+public class UserServiceTests
+{
+    private readonly IUserService _userService;
+    private readonly Mock<IUserRepository> _fakeUserRepository;
+    private readonly Mock<ITokenRepository> _tokenRepository;
+
+    public UserServiceTests()
+    {
+        _fakeUserRepository = new Mock<IUserRepository>();
+        _tokenRepository = new Mock<ITokenRepository>();
+        _userService = new UserService(_fakeUserRepository.Object, _tokenRepository.Object);
+    }
+}
