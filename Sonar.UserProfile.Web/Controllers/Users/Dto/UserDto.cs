7aff96d8b8da9adadc90e9339d249da48deb550a
Aedilay <Edgar.saratovcev@gmail.com>
2022-06-24 22:33:54 +0300

    feat: add email validation in UserDto

--- Sonar.UserProfile.Web/Controllers/Users/Dto/UserDto.cs
+++ Sonar.UserProfile.Web/Controllers/Users/Dto/UserDto.cs
@@ -1,7 +1,12 @@
-﻿namespace Sonar.UserProfile.Web.Controllers.Users.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sonar.UserProfile.Web.Controllers.Users.Dto;
 
 public class UserDto
 {
     public Guid Id { get; set; }
+    
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email is incorrect")]
     public string Email { get; set; }
 }
