03e59c61a22fb90e98c59d1563bf77f38fed825e
Valery Shevchenko <ar4er.valera@gmail.com>
2022-06-24 09:52:52 +0300

    refactor: remove public from interface

--- Sonar.UserProfile.ApiClient/Interfaces/IRelationshipApiClient.cs
+++ Sonar.UserProfile.ApiClient/Interfaces/IRelationshipApiClient.cs
@@ -7,7 +7,7 @@ public interface IRelationshipApiClient
     Task SendFriendshipRequestAsync(string token, string targetUserEmail, CancellationToken cancellationToken);
     Task<IReadOnlyList<UserDto>> GetFriendsAsync(string token, CancellationToken cancellationToken);
     Task<IReadOnlyList<UserDto>> GetRequestsFromMe(string token, CancellationToken cancellationToken);
-    public Task<IReadOnlyList<UserDto>> GetRequestsToMe(string token, CancellationToken cancellationToken);
+    Task<IReadOnlyList<UserDto>> GetRequestsToMe(string token, CancellationToken cancellationToken);
     Task AcceptFriendshipRequestAsync(string token, string requestedEmail, CancellationToken cancellationToken);
     Task RejectFriendshipRequestAsync(string token, string requestedEmail, CancellationToken cancellationToken);
 }
