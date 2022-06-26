b548d17d89bdaf6cc14f6ce5226f668c2e2ac966
nikolaykochubeev <nikolay.kochubeev@gmail.com>
2022-06-27 00:12:37 +0300

    refactor

--- Sonar.UserProfile.Core/Domain/SmtpClients/Services/ISmtpClientService.cs
+++ Sonar.UserProfile.Core/Domain/SmtpClients/Services/ISmtpClientService.cs
@@ -5,5 +5,5 @@ namespace Sonar.UserProfile.Core.Domain.SmtpClients.Services;
 public interface ISmtpClientService
 {
     Task<MailMessage> CreateMailMessageAsync(string email, string subject, string body, CancellationToken cancellationToken);
-    Task<bool> SendMailMessageAsync(MailMessage mailMessage, string userState, CancellationToken cancellationToken);
+    Task SendMailMessageAsync(MailMessage mailMessage, CancellationToken cancellationToken);
 }
