7ac946ed499899fe89a25de4742fd840c79822e0
nikolaykochubeev <nikolay.kochubeev@gmail.com>
2022-06-26 15:15:44 +0300

    feat: added RelationshipValidator.cs

---
+++ Sonar.UserProfile.Core/Domain/Users/Validators/RelationshipValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using Sonar.UserProfile.Core.Domain.Users.ValueObjects;
+
+namespace Sonar.UserProfile.Core.Domain.Users.Validators;
+
+public class RelationshipValidator : AbstractValidator<RelationshipStatus>
+{
+    public RelationshipValidator()
+    {
+    }
+}
