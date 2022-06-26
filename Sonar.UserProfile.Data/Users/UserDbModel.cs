e9b7184c8acb5e18cf76a77fbe75d88628a7d98e
Aedilay <Edgar.saratovcev@gmail.com>
2022-06-26 12:34:49 +0300

    feat: add field to UserDBModel

--- Sonar.UserProfile.Data/Users/UserDbModel.cs
+++ Sonar.UserProfile.Data/Users/UserDbModel.cs
@@ -1,9 +1,12 @@
-﻿namespace Sonar.UserProfile.Data.Users
+﻿using Sonar.UserProfile.Core.Domain.Users.ValueObjects;
+
+namespace Sonar.UserProfile.Data.Users
 {
     public class UserDbModel
     {
         public Guid Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public AccountType AccountType { get; set; }
     }
 }
