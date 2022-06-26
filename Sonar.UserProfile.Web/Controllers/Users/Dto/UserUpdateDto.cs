b97d4282dd541a4ada98e3f0f3964fc732dc88ad
Aedilay <Edgar.saratovcev@gmail.com>
2022-06-26 12:45:42 +0300

    feat: add new DTO for updating

---
+++ Sonar.UserProfile.Web/Controllers/Users/Dto/UserUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Sonar.UserProfile.Core.Domain.Users.ValueObjects;
+
+namespace Sonar.UserProfile.Web.Controllers.Users.Dto;
+
+public class UserUpdateDto
+{
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email is incorrect")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+    public string Password { get; set; }
+
+    public AccountType AccountType { get; set; }
+}
