7543e88f351960650cc96d158c12c89e61653587
nikolaykochubeev <nikolay.kochubeev@gmail.com>
2022-06-26 15:15:52 +0300

    feat: added UserValidator.cs

---
+++ Sonar.UserProfile.Core/Domain/Users/Validators/UserValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Sonar.UserProfile.Core.Domain.Users.Validators;
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty");
+    }
+}
